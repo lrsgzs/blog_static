b444859176c1718d7bf193a102d37922c997395e
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-10-17 15:14:39 +0800

    update cover

---
+++ cover/info.js
@@ -0,0 +1,19 @@
+// 标题
+var website_title = "lrs的主页"
+
+// 代表作
+var topics = [
+    [
+        "https://code.xueersi.com/home/project/detail?lang=code&pid=37977468&version=python&form=python&langType=python",
+        "mc服务器"
+    ],
+    [
+        "https://code.xueersi.com/home/project/detail?lang=code&pid=37922392&version=offline&form=python&langType=python",
+        "便携编辑器"
+    ]
+]
+
+// 消息
+var news = [
+    '好消息！！！详细请见<a href="https://code.xueersi.com/home/project/detail?lang=code&pid=37977468&version=python&form=python&langType=python">mc服务器</a>'
+]
