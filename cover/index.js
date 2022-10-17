06177899e25dd3d1ed16785be6f5efaa8d4c841a
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-10-17 15:17:00 +0800

    update cover js

---
+++ cover/index.js
@@ -0,0 +1,48 @@
+// 标题
+document.title = website_title;
+
+var doctitle = document.getElementById("title");
+doctitle.innerHTML = website_title;
+
+
+// 代表作
+var sub_biaoqian = document.getElementById("topics");
+
+for (var i = 0; i < topics.length; i++){
+    // 创建链接
+    var biaoqian = document.createElement("a");
+    
+    biaoqian.href = topics[i][0];
+    biaoqian.innerHTML = topics[i][1];
+    
+    sub_biaoqian.appendChild(biaoqian);
+    
+    
+    // 空白
+    var biaoqian = document.createElement("mcyyds");
+    
+    biaoqian.innerHTML = "  "
+    
+    sub_biaoqian.appendChild(biaoqian);
+}
+
+
+// 消息
+var sub_biaoqian = document.getElementById("news");
+
+for (var i = 0; i < news.length; i++){
+    // 显示消息
+    var biaoqian = document.createElement("a");
+    
+    biaoqian.innerHTML = news[i];
+    
+    sub_biaoqian.appendChild(biaoqian);
+    
+    
+    // 空白
+    var biaoqian = document.createElement("mcyyds");
+    
+    biaoqian.innerHTML = "  "
+    
+    sub_biaoqian.appendChild(biaoqian);
+}
