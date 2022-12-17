18b7e66169195fb0c58a990ab240f18b528c9cee
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-12-17 14:44:32 +0800

    upload some files

--- cover/info.js
+++ cover/info.js
@@ -15,5 +15,6 @@ var topics = [
 
 // 消息
 var news = [
-    '好消息！！！详细请见<a href="https://code.xueersi.com/home/project/detail?lang=code&pid=37977468&version=python&form=python&langType=python">mc服务器</a>'
+    '好消息！！！详细请见<a href="https://code.xueersi.com/home/project/detail?lang=code&pid=37977468&version=python&form=python&langType=python">《mc服务器》</a>',
+    '杨柳工作室招人啦！！！详细请见<a href="https://code.xueersi.com/home/project/detail?lang=code&pid=39572680&version=webpy&form=webpy&langType=webpy">《杨柳工作室成立！招募中！已设立网站地址在作品里》</a>'
 ]
